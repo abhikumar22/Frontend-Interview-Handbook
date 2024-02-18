f91d431e405c70ff19d249af059455a6330b793a
abhishek kumar <abhishekkumar@abhisheks-MacBook-Pro.local>
2024-02-18 16:25:28 +0530

    fix: added n task in parallel with k limit question

--- DSA/DSA/myScript.js
+++ DSA/DSA/myScript.js
@@ -295,3 +295,92 @@
 //   }
   
 //   console.log(deepclone(obj))
+
+
+
+// Execute N task in series with K limit
+// Todo to do cleanup and return once only
+
+// const delayFn = (delay) => {
+//     return function() {
+//       console.log("start ", delay)
+//       return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//           resolve(delay)
+//         }, delay)
+//       })
+//     }
+//   }
+//   const allTasks = [
+//     delayFn(2000),
+//     delayFn(2000),
+  
+//     delayFn(3000),
+//     delayFn(3000),
+  
+//     delayFn(4000),
+//     delayFn(4000),
+//   ]
+//   const executeInParallel = (allTasks, limit) => {
+//     let taskArr = [...allTasks];
+//     let count = 0;
+//     let results = [];
+//     let queueTask = [];
+//     let taskExecuting = 0;
+  
+//     function executeTasks(eachTasks, res) {
+//       eachTasks()
+//         .then((data) => {
+//           taskArr.shift();
+//           results.push(data);
+//           taskExecuting--;
+//           if (results.length < allTasks.length) {
+
+//             // type 1 (get All pending task and push again)
+//             let pendingTask = [...queueTask];
+//             queueTask = [];
+//             startTask(pendingTask)
+
+//             // type2 (take one task and add in queue)
+//             // if(!(queueTask && queueTask.length<=0)){
+//             //     console.log("******queueTask before",{queueTask});
+//             //     let pendingTask = queueTask.shift();
+//             //     console.log("******queueTask after",{queueTask},{pendingTask});
+//             //     startTask([pendingTask])
+//             // }
+//           } else {
+//             console.log("**** finallyyyyyyyyyyy",{time:new Date()}, {results});
+//             res(6);
+//             return;
+//           }
+//         })
+//       // .catch((err) => {
+//       // })
+//       // .finally(() => {
+//       // })
+//     }
+//     function startTask(taskToExecute) {
+//       return new Promise((resolve, reject) => {
+//         taskToExecute.forEach(element => {
+//           if (taskExecuting < limit) {
+//             // count = count + 1;
+//             taskExecuting++;
+//             executeTasks(element, resolve);
+//           }
+//           else {
+//             queueTask.push(element);
+//           }
+//         });
+//       })
+//     }
+//     console.log("**** Start", new Date())
+//     startTask(taskArr);
+//   }
+  
+//   executeInParallel(allTasks, 3)
+//     .then((data) => {
+//       console.log("**** then ", data)
+//     })
+//     .catch((error) => {
+//       console.log("**** error ", error)
+//     })
